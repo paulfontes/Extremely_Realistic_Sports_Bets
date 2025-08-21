70aa2ede27e32a6fdd23fa9be38d770fe301464e
Paul Fontes <pgfon22@gmail.com>
2025-08-20 20:10:42 -0600

    more even teams math.floor?

--- app.js
+++ app.js
@@ -3,16 +3,16 @@ let bank = 100;
 // let teamOneElementID = document.getElementById('team1')
 
 const players = [
-    { teamNumber: 1, emoji: "🏃‍♂️", skill: 10, name: "D'Marcus Williums" },
+    { teamNumber: 1, emoji: "🏃‍♂️", skill: 100, name: "D'Marcus Williums" },
     { teamNumber: 1, emoji: "🤾‍♂️", skill: 30, name: "Tyroil Smoochie-Wallace" },
     { teamNumber: 1, emoji: "🏇", skill: 88, name: "Jackmerius Tacktheratrix" },
-    { teamNumber: 1, emoji: "🏌️‍♀️", skill: 15, name: "Javaris Jamar Javarison-Lamar", },
+    { teamNumber: 1, emoji: "🏌️‍♀️", skill: 50, name: "Javaris Jamar Javarison-Lamar", },
     { teamNumber: 1, emoji: "🏋️‍♂️", skill: 77, name: "D'Pez Poopsie" },
-    { teamNumber: 1, emoji: "🏌️‍♂️", skill: 21, name: "D'Jasper Probincrux III" },
-    { teamNumber: 1, emoji: "🤾", skill: 5, name: "Leoz Maxwell Jilliumz" },
+    { teamNumber: 1, emoji: "🏌️‍♂️", skill: 77, name: "D'Jasper Probincrux III" },
+    { teamNumber: 1, emoji: "🤾", skill: 50, name: "Leoz Maxwell Jilliumz" },
     { teamNumber: 1, emoji: "🏂", skill: 99, name: "Hingle McCringleberry" },
     { teamNumber: 1, emoji: "🧘‍♀️", skill: 50, name: "L'Carpetron Dookmarriot" },
-    { teamNumber: 1, emoji: "🚶‍♀️", skill: 1, name: "Xmus Jaxon Flaxon-Waxon" },
+    { teamNumber: 1, emoji: "🚶‍♀️", skill: 60, name: "Xmus Jaxon Flaxon-Waxon" },
     { teamNumber: 2, emoji: "🏋️‍♀️", skill: 61, name: "Saggitariutt Jefferspin" },
     { teamNumber: 2, emoji: "🤺", skill: 34, name: "Quatro Quatro" },
     { teamNumber: 2, emoji: "🏄", skill: 71, name: "X-Wing @Aliciousness" },
@@ -21,7 +21,7 @@ const players = [
     { teamNumber: 2, emoji: "⛹️‍♀️", skill: 23, name: "Donkey Teeth" },
     { teamNumber: 2, emoji: "🕴️", skill: 58, name: "T.J. A.J. R.J. Backslashinfourth V", },
     { teamNumber: 2, emoji: "💃", skill: 99, name: "Firstname Lastname" },
-    { teamNumber: 2, emoji: "🧍‍♂️", skill: 3, name: "Dan Smith" },
+    { teamNumber: 2, emoji: "🧍‍♂️", skill: 30, name: "Dan Smith" },
     { teamNumber: 2, emoji: "🐅", skill: 100, name: "Tiger" },
 ];
 function draftPlayers() {
@@ -30,7 +30,7 @@ function draftPlayers() {
 
     let draftTeamNumber = [1,2]
     players.forEach((player) => {
-        let randomPosition = Math.round(Math.random() * 2)
+        let randomPosition = Math.floor(Math.random() * 2)
         player.teamNumber = draftTeamNumber[randomPosition]
     })
     console.log('Drafting Teams')
